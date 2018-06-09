92cb984d494167474c3343d5a3c52e7aa3f494c4
Arseniy Tashoyan <tashoyan@gmail.com>
2018-06-09 23:52:26 +0300

    Initial commit

---
+++ build.sbt
@@ -0,0 +1,75 @@
+
+name := "spark-sql-tfidf"
+version := "0.1-SNAPSHOT"
+scalaVersion := "2.11.12"
+
+scalacOptions ++= Seq(
+  "-deprecation",
+  "-encoding", "UTF-8",
+  "-feature",
+  "-target:jvm-1.8",
+  "-unchecked",
+  "-Ywarn-adapted-args",
+  "-Ywarn-dead-code",
+  "-Ywarn-numeric-widen",
+  "-Ywarn-unused",
+  "-Ywarn-value-discard",
+  "-Xfuture",
+  "-Xlint"
+)
+
+javacOptions ++= Seq(
+  "-Xlint:deprecation",
+  "-Xlint:unchecked",
+  "-source", "1.8",
+  "-target", "1.8",
+  "-g:vars"
+)
+
+val sparkVersion = "2.2.1"
+
+libraryDependencies ++= Seq(
+//  "org.apache.spark" %% "spark-core" % sparkVersion,
+  "org.apache.spark" %% "spark-sql" % sparkVersion % "provided",
+  "org.scalatest" %% "scalatest" % "3.0.4" % "test"
+)
+
+fork := true
+
+import sbtassembly.MergeStrategy
+assemblyMergeStrategy in assembly := { // this is the default plus one more for mime.types
+  // See https://github.com/sbt/sbt-assembly#merge-strategy
+  case x if Assembly.isConfigFile(x) =>
+    MergeStrategy.concat
+
+  case PathList(ps @ _*) if Assembly.isReadme(ps.last) || Assembly.isLicenseFile(ps.last) =>
+    MergeStrategy.rename
+
+  case PathList(ps @ _*) if ps.last.startsWith("CHANGELOG.") =>
+    MergeStrategy.discard
+
+  case PathList("META-INF", xs @ _*) =>
+    xs map {_.toLowerCase} match {
+      case "mime.types" :: _ =>
+        MergeStrategy.filterDistinctLines
+
+      case "manifest.mf" :: Nil | "index.list" :: Nil | "dependencies" :: Nil =>
+        MergeStrategy.discard
+
+      case ps @ x :: _ if ps.last.endsWith(".sf") || ps.last.endsWith(".dsa") =>
+        MergeStrategy.discard
+
+      case "plexus" :: _ =>
+        MergeStrategy.discard
+
+      case "services" :: _ =>
+        MergeStrategy.filterDistinctLines
+
+      case "spring.schemas" :: Nil | "spring.handlers" :: Nil =>
+        MergeStrategy.filterDistinctLines
+
+      case _ => MergeStrategy.deduplicate
+    }
+
+  case _ => MergeStrategy.deduplicate
+}
---
+++ src/main/scala/com/github/tashoyan/tfidf/TfIdf.scala
@@ -0,0 +1,87 @@
+package com.github.tashoyan.tfidf
+
+import com.github.tashoyan.tfidf.TfIdfSpark._
+import org.apache.spark.sql.DataFrame
+import org.apache.spark.sql.functions._
+
+/**
+  * Utility to calculate TF * IDF metrics for tokens in documents.
+  *
+  * @param config Configuration object that allows to customize data set column names.
+  */
+class TfIdf(config: TfIdfSparkConfig) {
+
+  /**
+    * Calculates TF, DF, IDF and TF * IDF metrics for all tokens from the input document set.
+    *
+    * @param documents Data set with documents.
+    *                  The column containing documents is defined by [[TfIdfSparkConfig.documentColumn]] parameter.
+    * @return Data set with the following columns (column names are configurable via [[TfIdfSparkConfig]] config object):
+    *         <ul>
+    *         <li> `document` - input documents
+    *         <li> `doc_id` - unique identifier for the each input document
+    *         <li> `token` - one token from this document (specified in the `document` column)
+    *         <li> `tf` - term frequency of this token within this document
+    *         <li> `df` - document frequency of this token across all documents
+    *         <li> `idf` - inverse document frequency of this token across all documents
+    *         <li> `tf_idf` - TF * IDF of this token for this document
+    *         </ul>
+    *         Other columns, if any, are retained from the input data set.
+    */
+  def genTfIdf(documents: DataFrame): DataFrame = {
+    val docsWithId = addDocId(documents)
+    val unfoldedDocs = unfoldDocs(docsWithId)
+    val tokensWithTf = addTf(unfoldedDocs)
+    val tokensWithDf = addDf(unfoldedDocs)
+    val tokensWithDfIdf = addIdf(tokensWithDf, documents.count())
+    val tfIdf = joinTfIdf(tokensWithTf, tokensWithDfIdf)
+
+    unfoldedDocs
+      .join(tfIdf, Seq(config.docIdColumn, config.tokenColumn), "left")
+  }
+
+  protected def addDocId(documents: DataFrame): DataFrame =
+    documents.withColumn(config.docIdColumn, monotonically_increasing_id())
+
+  protected def unfoldDocs(documents: DataFrame): DataFrame = {
+    val columns = documents.columns.map(col) :+
+      (explode(col(config.documentColumn)) as config.tokenColumn)
+    documents.select(columns: _*)
+  }
+
+  protected def addTf(unfoldedDocs: DataFrame): DataFrame =
+    unfoldedDocs.groupBy(config.docIdColumn, config.tokenColumn)
+      .agg(count(config.documentColumn) as config.tfColumn)
+
+  protected def addDf(unfoldedDocs: DataFrame): DataFrame =
+    unfoldedDocs.groupBy(config.tokenColumn)
+      .agg(countDistinct(config.docIdColumn) as config.dfColumn)
+
+  protected def addIdf(tokensWithDf: DataFrame, docCount: Long): DataFrame = {
+    val calcIdfUdf = udf { df: Long => calcIdf(docCount, df) }
+    tokensWithDf.withColumn(config.idfColumn, calcIdfUdf(col(config.dfColumn)))
+  }
+
+  protected def joinTfIdf(tokensWithTf: DataFrame, tokensWithDfIdf: DataFrame): DataFrame =
+    tokensWithTf
+      .join(tokensWithDfIdf, Seq(config.tokenColumn), "left")
+      .withColumn(config.tfIdfColumn, col(config.tfColumn) * col(config.idfColumn))
+
+}
+
+case class TfIdfSparkConfig(
+    documentColumn: String = "document",
+    docIdColumn: String = "doc_id",
+    tokenColumn: String = "token",
+    tfColumn: String = "tf",
+    dfColumn: String = "df",
+    idfColumn: String = "idf",
+    tfIdfColumn: String = "tf_idf"
+)
+
+object TfIdfSpark {
+
+  def calcIdf(docCount: Long, df: Long): Double =
+    math.log((docCount.toDouble + 1) / (df.toDouble + 1))
+
+}
