176f74090d68f23ea2303a9798d4ce7bc9c4ba2c
Arseniy Tashoyan <tashoyan@gmail.com>
2018-06-10 13:55:40 +0300

    Don't hardcode songs, read all docs from a directory. Remove abbreviations like I'll

--- src/main/scala/com/github/tashoyan/tfidf/Main.scala
+++ src/main/scala/com/github/tashoyan/tfidf/Main.scala
@@ -1,5 +1,7 @@
 package com.github.tashoyan.tfidf
 
+import java.io.File
+
 import org.apache.spark.ml.feature.RegexTokenizer
 import org.apache.spark.sql.expressions.Window
 import org.apache.spark.sql.functions._
@@ -16,15 +18,23 @@ object Main {
   import spark.implicits._
 
   private val docNameColumn = "doc_name"
+  private val docPathColumn = "doc_path"
   private val rawTextColumn = "raw_text"
   private val wordsColumn = "words"
 
   def main(args: Array[String]): Unit = {
-    val documents = readDocuments(
-      "battle_hymn_of_the_republic.txt",
-      "seek_and_destroy.txt",
-      "love_me_tender.txt"
-    )
+    if (args.length < 1) {
+      throw new IllegalArgumentException("Argument is expected: directory with documents database")
+    }
+    val docsDirPath = args.head
+
+    val documents = readDocuments(docsDirPath)
+
+    //    val documents = readDocuments(
+    //      "battle_hymn_of_the_republic.txt",
+    //      "seek_and_destroy.txt",
+    //      "love_me_tender.txt"
+    //    )
 
     val words = prepareWords(documents)
     words
@@ -45,23 +55,33 @@ object Main {
       .show(100, truncate = false)
   }
 
-  private def readDocuments(names: String*): DataFrame = {
-    val docs: Seq[(String, String)] = names.map(readDocument)
-    docs.toDF(docNameColumn, rawTextColumn)
-  }
+  private def readDocuments(docsDirPath: String): DataFrame = {
+    val docsDir = new File(docsDirPath)
+    if (!docsDir.isDirectory) {
+      throw new IllegalArgumentException(s"Not a directory: $docsDirPath")
+    }
 
-  private def readDocument(name: String): (String, String) = {
-    val resourceStream = this.getClass
-      .getResourceAsStream(name)
-    val content = Source.fromInputStream(resourceStream)
-      .mkString
-    (name, content)
+    val docFiles = docsDir.listFiles()
+      .filter(_.isFile)
+    if (docFiles.isEmpty) {
+      throw new IllegalArgumentException(s"None files found in the directory: $docsDirPath")
+    }
+
+    docFiles.map { docFile =>
+      (docFile.getName, docFile.getAbsolutePath, Source.fromFile(docFile).mkString)
+    }
+      .toSeq
+      .toDF(docNameColumn, docPathColumn, rawTextColumn)
   }
 
   private def prepareWords(rawText: DataFrame): DataFrame = {
+    val noAbbrColumn = "no_abbr"
+    val noAbbrText = rawText.withColumn(noAbbrColumn,
+      regexp_replace(col(rawTextColumn), """\w+'ll""", ""))
+
     val noPunctColumn = "no_punct"
-    val noPunctText = rawText.withColumn(noPunctColumn,
-      regexp_replace(col(rawTextColumn), """[\p{Punct}]""", ""))
+    val noPunctText = noAbbrText.withColumn(noPunctColumn,
+      regexp_replace(col(noAbbrColumn), """[\p{Punct}]""", ""))
 
     val tokenizer: RegexTokenizer = new RegexTokenizer()
       .setInputCol(noPunctColumn)
