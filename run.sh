af9abdb40ce445f2788669fef075f1f18c6c351d
Arseniy Tashoyan <tashoyan@gmail.com>
2018-06-11 22:07:41 +0300

    Launch script

---
+++ run.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+set -o nounset
+set -o errexit
+
+work_dir="$(cd "$(dirname -- "$0")" ; pwd)"
+
+$SPARK_HOME/bin/spark-submit \
+--master 'local[*]' \
+--driver-java-options "-Dlog4j.configuration=file://$work_dir/src/test/resources/log4j.xml" \
+--driver-memory 2G \
+--class com.github.tashoyan.tfidf.Main \
+target/scala-2.11/spark-sql-tfidf-assembly-0.1-SNAPSHOT.jar songs_db
